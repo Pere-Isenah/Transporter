75af4fa6be2bdca3b036f8d9eb9dfd228dbc0d24
Pere-Isenah <pereisenah@gmail.com>
2024-04-19 21:46:04 +0100

    updated address

--- src/components/Booking/AutoCompleteAddress.tsx
+++ src/components/Booking/AutoCompleteAddress.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { AddressAutofill } from '@mapbox/search-js-react';
+
 
 const AutoCompleteAddress = () => {
   return (
     <>
       <div className=' w-full py-3 px-2'>
     <label className="text-sm">Where from?</label>
-      <input className='border rounded-md outline-none p-3 w-full text-black shadow focus:border-black border text-sm'/>
+    <AddressAutofill accessToken="pk.eyJ1IjoicGpzaW1wc29uIiwiYSI6ImNsdjZ1ZG9xZzAyOWEyaW81OHdoZ2VpcnAifQ.X44UU75dQ9IteNfJa_Va5g">
+      <input className='border rounded-md outline-none p-3 w-full text-black shadow focus:border-black border text-sm' autoComplete="address-line1"/>
+    </AddressAutofill>
     </div>
     <div className=' w-full py-3 px-2'>
     <label className="text-sm">Where To?</label>
