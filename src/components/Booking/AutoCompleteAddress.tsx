8fb29ad2e0fece275d92b5cf150e53983b3c9026
Pere-Isenah <pereisenah@gmail.com>
2024-04-20 03:30:06 +0100

    update

--- src/components/Booking/AutoCompleteAddress.tsx
+++ src/components/Booking/AutoCompleteAddress.tsx
@@ -7,7 +7,7 @@ const AutoCompleteAddress = () => {
     <>
       <div className=' w-full py-3 px-2'>
     <label className="text-sm">Where from?</label>
-    <AddressAutofill accessToken="pk.eyJ1IjoicGpzaW1wc29uIiwiYSI6ImNsdjZ1ZG9xZzAyOWEyaW81OHdoZ2VpcnAifQ.X44UU75dQ9IteNfJa_Va5g">
+    <AddressAutofill accessToken="">
       <input className='border rounded-md outline-none p-3 w-full text-black shadow focus:border-black border text-sm' autoComplete="address-line1"/>
     </AddressAutofill>
     </div>
