56fdda4319da08dffd734881e3bccda4fa09c591
Pere-Isenah <pereisenah@gmail.com>
2024-04-20 03:36:26 +0100

    update

--- src/components/Booking/AutoCompleteAddress.tsx
+++ src/components/Booking/AutoCompleteAddress.tsx
@@ -3,11 +3,12 @@ import { AddressAutofill } from '@mapbox/search-js-react';
 
 
 const AutoCompleteAddress = () => {
+  const mapKey =process.env.MAP_BOX_TOKEN
   return (
     <>
       <div className=' w-full py-3 px-2'>
     <label className="text-sm">Where from?</label>
-    <AddressAutofill accessToken="">
+    <AddressAutofill accessToken={mapKey}>
       <input className='border rounded-md outline-none p-3 w-full text-black shadow focus:border-black border text-sm' autoComplete="address-line1"/>
     </AddressAutofill>
     </div>
