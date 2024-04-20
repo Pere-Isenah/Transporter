378d5161366a935b9a38fab0bfe82b1e246fe776
Pere-Isenah <pereisenah@gmail.com>
2024-04-20 04:39:23 +0100

    added

--- src/components/Booking/AutoCompleteAddress.tsx
+++ src/components/Booking/AutoCompleteAddress.tsx
@@ -1,24 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { AddressAutofill } from '@mapbox/search-js-react';
-
 
 const AutoCompleteAddress = () => {
   const MAP_KEY = import.meta.env.VITE_MAP_BOX_TOKEN_KEY
-  console.log(MAP_KEY);
+  const [address, setAddress] = useState('');
+
+  const handleSelect = (selected) => {
+    setAddress(selected.place_name);
+  };
+
   return (
     <>
       <div className=' w-full py-3 px-2'>
-    <label className="text-sm">Where from?</label>
-    <AddressAutofill accessToken={MAP_KEY}>
-      <input className='border rounded-md outline-none p-3 w-full text-black shadow focus:border-black border text-sm' autoComplete="address-line1"/>
-    </AddressAutofill>
-    </div>
-    <div className=' w-full py-3 px-2'>
-    <label className="text-sm">Where To?</label>
-      <input className='border rounded-md outline-none p-3 w-full text-black shadow focus:border-black border text-sm'/>
-    </div>
+        <label className="text-sm">Where from?</label>
+        <AddressAutofill accessToken={MAP_KEY} onSelect={handleSelect}>
+          <input
+            className='border rounded-md outline-none p-3 w-full text-black shadow focus:border-black border text-sm'
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+            autoComplete="address-line1"
+          />
+        </AddressAutofill>
+      </div>
+      <div className=' w-full py-3 px-2'>
+        <label className="text-sm">Where To?</label>
+        <input
+          className='border rounded-md outline-none p-3 w-full text-black shadow focus:border-black border text-sm'
+        />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default AutoCompleteAddress
+export default AutoCompleteAddress;
